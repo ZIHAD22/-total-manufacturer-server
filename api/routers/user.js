016ed70b62c7cf2d4fec198c01c780c7f33cbe79
ZIHAD22 <mdzihad2244@gmail.com>
2022-05-27 22:32:52 +0600

    add admin route and make admin implement

--- api/routers/user.js
+++ api/routers/user.js
@@ -3,6 +3,26 @@ const Users = require('../models/Users')
 
 const users = Router()
 
+users.get('/all', async (req, res) => {
+  const result = await Users.find()
+
+  res.json(result)
+})
+users.patch('/all/:id', async (req, res) => {
+  const _id = req.params.id
+  const result = await Users.findOneAndUpdate({ _id }, { role: 'admin' })
+
+  res.send(result)
+})
+
+users.get('/all/admin/:email', async (req, res) => {
+  const userEmail = req.params.email
+  const user = await Users.findOne({ userEmail })
+  const isAdmin = user.role === 'admin'
+
+  res.json({ admin: isAdmin })
+})
+
 users.get('/:email', async (req, res) => {
   const { email } = req.params
   const result = await Users.findOne({ userEmail: email })
